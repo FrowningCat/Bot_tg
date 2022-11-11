f8fd908ef3d915195eaaea9589fd6db25d4d9964
FrowningCat <vsavinkov60@gmail.com>
2022-11-11 19:15:24 +0300

    add_last_version

---
+++ strings.py
@@ -0,0 +1,16 @@
+BOT_TOKEN_FILENAME = 'token.txt'
+
+SYMBOL_X = '❌'
+SYMBOL_O = '⭕'
+SYMBOL_UNDEF = '◻'
+
+ANSW_YOUR_TURN = 'Ваш ход'
+ANSW_YOU_WIN = 'Вы победили'
+ANSW_BOT_WIN = 'Победил бот'
+ANSW_DRAW = 'Ничья'
+ANSW_HELP = 'Бот для игры в крестики-нолики. Пожалуйста, нажимайте на поля, не нужно писать в чат.\n\n' \
+            'Используйте команду /new_game для начала новой игры\n' \
+            'Используйте команду /author для того что бы узнать кто автор'
+ANSW_AUTH = 'Создатель бота @Frowning_Cat'
+
+ALERT_CANNOT_MOVE_TO_THIS_CELL = 'Нажимать можно только на ' + SYMBOL_UNDEF
